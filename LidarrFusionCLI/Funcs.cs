148b56b5fbd57cb4b1b7fb94a2301b7343f32fe4
BuildTools <unconfigured@null.spigotmc.org>
2024-02-16 03:01:22 -0500

    Add project files.

---
+++ LidarrFusionCLI/Funcs.cs
@@ -0,0 +1,294 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.RegularExpressions;
+using TagLib;
+
+class Funcs
+{
+    public static string TrimDatesinSquareBrackets(string input)
+    {
+        string pattern = @"\[\d+\]";
+        string result = Regex.Replace(input, pattern, "");
+        return result.Trim();
+    }
+    public static string FindClosestMatch(string input, dynamic allAlbums)
+    {
+        string closestMatch = null;
+        int minDistance = int.MaxValue;
+
+        foreach (var album in allAlbums)
+        {
+            string candidate = album.title;
+
+            int distance = LevenshteinDistance(input, candidate);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestMatch = album.id;
+            }
+        }
+
+        return closestMatch;
+    }
+    private static int LevenshteinDistance(string s, string t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+        int[,] d = new int[n + 1, m + 1];
+        if (n == 0)
+            return m;
+        if (m == 0)
+            return n;
+        for (int i = 0; i <= n; d[i, 0] = i++) { }
+        for (int j = 0; j <= m; d[0, j] = j++) { }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                d[i, j] = Math.Min(
+                    Math.Min(d[i - 1, j] + 1,
+                    d[i, j - 1] + 1),
+                    d[i - 1, j - 1] + cost);
+                if (i > 1 && j > 1 && s[i - 1] == t[j - 2] && s[i - 2] == t[j - 1])
+                {
+                    d[i, j] = Math.Min(d[i, j], d[i - 2, j - 2] + cost);
+                }
+            }
+        }
+        return d[n, m];
+    }
+    public static string SearchYoutube(string query)
+    {
+        string queryEncoded = WebUtility.UrlEncode(query);
+        string url = "https://www.youtube.com/results?search_query=" + queryEncoded;
+        using (var client = new WebClient())
+        {
+            try
+            {
+                string response = client.DownloadString(url);
+                if (response != null)
+                {
+                    var matches = Regex.Match(response, "\"videoRenderer\"\\s*:\\s*{\"videoId\"\\s*:\\s*\"([^\"]+)\"");
+                    if (matches.Success)
+                    {
+                        string videoId = matches.Groups[1].Value;
+                        string videoUrl = "https://www.youtube.com/watch?v=" + videoId;
+                        System.Diagnostics.Debug.WriteLine("Video link found: " + videoUrl);
+                        return videoUrl;
+                    }
+                    else
+                    {
+                        return "Video link not found in the search results.";
+                    }
+                }
+                else
+                {
+                    return "Error fetching YouTube search results.";
+                }
+            }
+            catch (WebException ex)
+            {
+                return "Error fetching YouTube search results: " + ex.Message;
+            }
+        }
+    }
+    public static string GetRandomInsult()
+    {
+        List<string> insults = new List<string>
+        {
+            "Usa tu cerebro, es gratis",
+            "Eres más soso que la comida de un astronauta",
+            "Eres más feo que una carretilla con pegatinas",
+            "Eres una inversión a fondo perdido",
+            "Te insultaría pero no lo haría tan bien como lo hizo la naturaleza",
+            "Contigo se confirma la teoría científica de que un humano puede vivir sin cerebro",
+            "Si los zombies comen cerebros, tú serías invisible para ellos",
+            "Te daré dos medallas, una por tonto y la otra por si la pierdes",
+            "Eres más lento que una tortuga con muletas",
+            "Tienes menos luces que un árbol de Navidad en enero"
+        };
+
+        Random random = new Random();
+        int index = random.Next(insults.Count);
+        return insults[index];
+    }
+
+    public static Albuminfo GetAlbuminfo(string artist, string album)
+    {
+        string apiUrl = "https://musicbrainz.org/ws/2/";
+        string formattedArtist = WebUtility.UrlEncode(artist);
+        string formattedAlbum = WebUtility.UrlEncode(album);
+        string url = $"{apiUrl}release/?query=artist:\"{formattedArtist}\"+AND+release:\"{formattedAlbum}\"&fmt=json";
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+        request.UserAgent = "Listenarr V1";
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            string jsonResponse = reader.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(jsonResponse))
+            {
+                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonResponse);
+                Albuminfo Albuminfo = new Albuminfo();
+                
+                Albuminfo.formattedArtist = data.releases[0]["artist-credit"][0].name;
+                Albuminfo.Country = data.releases[0].country;
+                Albuminfo.lang = data.releases[0]["rext-representation"].language;
+                Albuminfo.status = data.releases[0].status;
+                Albuminfo.Date = data.releases[0].date;
+                Albuminfo.formattedtitle = data.releases[0].title;
+                Albuminfo.score = data.releases[0].score;
+                Albuminfo.id = data.releases[0].id;
+
+                return Albuminfo;
+            }
+        }
+        return null;
+    }
+
+    public static string[] GetTrackList(string artist, string album)
+    {
+        string apiUrl = "https://musicbrainz.org/ws/2/";
+        string formattedArtist = WebUtility.UrlEncode(artist);
+        string formattedAlbum = WebUtility.UrlEncode(album);
+        string url = $"{apiUrl}release/?query=artist:\"{formattedArtist}\"+AND+release:\"{formattedAlbum}\"&fmt=json";
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+        request.UserAgent = "Listenarr V1";
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            string jsonResponse = reader.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(jsonResponse))
+            {
+                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonResponse);
+
+                if (data.releases != null && data.releases.Count > 0)
+                {
+                    string releaseId = data.releases[0].id;
+                    string tracklistUrl = $"{apiUrl}release/{releaseId}?inc=recordings&fmt=json";
+
+                    request = (HttpWebRequest)WebRequest.Create(tracklistUrl);
+                    request.Method = "GET";
+                    request.UserAgent = "Listenarr V1";
+
+                    using (HttpWebResponse tracklistResponse = (HttpWebResponse)request.GetResponse())
+                    using (Stream tracklistStream = tracklistResponse.GetResponseStream())
+                    using (StreamReader tracklistReader = new StreamReader(tracklistStream))
+                    {
+                        string tracklistJsonResponse = tracklistReader.ReadToEnd();
+
+                        if (!string.IsNullOrEmpty(tracklistJsonResponse))
+                        {
+                            dynamic tracklistData = Newtonsoft.Json.JsonConvert.DeserializeObject(tracklistJsonResponse);
+
+                            if (tracklistData.media[0].tracks != null)
+                            {
+                                IEnumerable<dynamic> tracks = (IEnumerable<dynamic>)tracklistData.media[0].tracks;
+                                return tracks.Select(t => (string)t.title).ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void PrintMinorHeader(string minorHeaderText)
+    {
+        string thinLine = new string('-', 60);
+        string spaceLine = new string(' ', 60);
+
+        Console.WriteLine();
+        Console.WriteLine($"+{thinLine}+");
+        Console.WriteLine($"|{spaceLine}|");
+        Console.WriteLine($"|{minorHeaderText.ToUpper(),60}|");
+        Console.WriteLine($"|{spaceLine}|");
+        Console.WriteLine($"+{thinLine}+");
+        Console.WriteLine();
+    }
+
+     static string GetLidarrRootFolderPath(string lidarrApiUrl, string lidarrApiKey)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("X-Api-Key", lidarrApiKey);
+            try
+            {
+                HttpResponseMessage response = client.GetAsync($"{lidarrApiUrl}/api/v1/rootfolder").Result;
+                response.EnsureSuccessStatusCode(); // Throws exception for non-success status codes
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                RootFolder[] rootFolders = Newtonsoft.Json.JsonConvert.DeserializeObject<RootFolder[]>(jsonResponse);
+                if (rootFolders.Length > 0)
+                {
+                    return rootFolders[0].path;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            return null;
+        }
+    }
+
+    public static void WriteID3v3Tags(string filePath, string artist, string album, string track)
+    {
+        try
+        {
+            using (var file = TagLib.File.Create(filePath))
+            {
+                var tag = (TagLib.Id3v2.Tag)file.GetTag(TagTypes.Id3v2, true);
+
+                tag.Title = track;
+                tag.Artists = new[] { artist };
+                tag.Album = album;
+                file.Save();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error writing ID3v3 tags: {ex.Message}");
+        }
+    }
+
+    public static string SanitizePath(string path)
+    {
+        char[] invalidChars = Path.GetInvalidFileNameChars();
+        string sanitizedPath = new string(path
+            .Where(c => !invalidChars.Contains(c))
+            .ToArray());
+
+        return sanitizedPath.Replace("<","").Replace(">","").Replace("\"","");
+    }
+}
+class RootFolder
+{
+    public string path { get; set; }
+}
+
+class Albuminfo
+{
+    public string formattedtitle { get; set; } 
+    public string status { get; set; } 
+    public string lang { get; set; } 
+    public string Date { get; set; } 
+    public string Country { get; set; } 
+    public string formattedArtist { get; set; } 
+    public string score { get; set; }
+    public string id { get; set; }
+
+}
---
+++ LidarrFusionCLI/Program.cs
@@ -0,0 +1,294 @@
+﻿using System.Diagnostics;
+using System.Net;
+using Figgle;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string lidarrApiUrl = null;
+        string lidarrApiKey = null;
+        bool autoDownload = false;
+        bool verbose = false;
+        bool tagger = true;
+        string generateList = null;
+        string rootpath = null;
+        string ytqappend = null;
+        string ytdlp = null;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i].ToLower())
+            {
+                case "-lidarrapiurl":
+                case "-lau":
+                    if (i + 1 < args.Length)
+                    {
+                        lidarrApiUrl = args[i + 1];
+                        i++;
+                    }
+                    break;
+
+                case "-lidarrapikey":
+                case "-lak":
+                    if (i + 1 < args.Length)
+                    {
+                        lidarrApiKey = args[i + 1];
+                        i++;
+                    }
+                    break;
+
+                case "-lidarrootpath":
+                case "-lrp":
+                    if (i + 1 < args.Length)
+                    {
+                        rootpath = args[i + 1];
+                        i++;
+                    }
+                    break;
+                case "-autodownload":
+                case "-adl":
+                    autoDownload = true;
+                    break;
+
+                case "-notagger":
+                case "-ntg":
+                    tagger = false;
+                    break;
+
+                case "-verbose":
+                case "-v":
+                    verbose = true;
+                    break;
+
+                case "-generate":
+                case "-gen":
+                    if (i + 1 < args.Length)
+                    {
+                        generateList = args[i + 1];
+                        i++;
+                    }
+                    break;
+
+                case "-ytdlp":
+                case "-ytd":
+                    if (i + 1 < args.Length)
+                    {
+                        ytdlp = args[i + 1];
+                        i++;
+                    }
+                    break;
+
+                case "-ytquery":
+                case "-ytq":
+                    if (i + 1 < args.Length)
+                    {
+                        ytqappend = args[i + 1];
+                        i++;
+                    }
+                    break;
+
+                case "-h":
+
+                    Console.WriteLine("Usage: <app> [options]");
+                    Console.WriteLine("-lidarrapiurl or -lau: Set Lidarr API URL (required)");
+                    Console.WriteLine("-lidarrapikey or -lak: Set Lidarr API Key (required))");
+                    Console.WriteLine("-lidarrootpath or -lrp: Set Lidarr root path (optional)");
+                    Console.WriteLine("-autodownload or -adl: Enable auto download (optional)");
+                    Console.WriteLine("-verbose or -v: Enable verbose mode (optional)" );
+                    Console.WriteLine("-generate or -gen: Generate batch gen (optional)");
+                    Console.WriteLine("-notagger or -ntg: turn off auto tagging (optional)");
+                    Console.WriteLine("-ytdlp or -ytd: Specify the binary for yt-dlp.exe");
+                    Console.WriteLine("-ytquery or -ytq: Appends query string on youtube search (Optional)");
+                    Console.WriteLine("-help or -h: Display this help message");
+                    return;
+            }
+        }
+
+        bool generateListFlag = !string.IsNullOrEmpty(generateList);
+
+        if (generateListFlag)
+        {
+            if(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,generateList))) {
+                File.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, generateList));
+                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, generateList), "@echo off" + Environment.NewLine);
+            }
+        }
+
+        Console.WriteLine($"lidarrApiUrl: {lidarrApiUrl}");
+        Console.WriteLine($"lidarrApiKey: {lidarrApiKey}");
+        if (verbose) { 
+            Console.WriteLine($"autoDownload: {autoDownload}");
+            Console.WriteLine($"verbose: {verbose}");
+            Console.WriteLine($"generateList: {generateList}");
+            Console.WriteLine($"Generate_List: {generateListFlag}");
+            Console.WriteLine($"YTQuery Append: \"{ytqappend}\"");
+        };
+        Console.WriteLine();
+
+        int estupidocounter = 0;
+    RetryCheck:
+
+        if (estupidocounter >= 10) {
+            Console.WriteLine(Funcs.GetRandomInsult());
+            Environment.Exit(0);
+        }
+
+        Console.WriteLine("Do your settings look correct? (y/n)");
+
+        ConsoleKeyInfo key = Console.ReadKey();
+
+        if (key.Key == ConsoleKey.Y)
+        {
+            Console.WriteLine("\nUser agreed. Continuing with the process...");
+            goto Contig;
+        }
+        else if (key.Key == ConsoleKey.N)
+        {
+            Console.WriteLine("\nUser declined. Exiting the process...");
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("\nInvalid input. Please press 'y' or 'n'.");
+            estupidocounter++;
+            goto RetryCheck;
+        }
+    Contig:
+        Console.WriteLine();
+        using (var client = new WebClient())
+        {
+            client.Headers.Add("X-Api-Key", lidarrApiKey);
+            string url = null;
+            if(rootpath == null) { 
+                url = $"{lidarrApiUrl}/api/v1/rootfolder";
+                if (verbose) { Console.WriteLine(url); Console.WriteLine(); };
+                try
+                {
+                    string response = client.DownloadString(url);
+                    dynamic root = Newtonsoft.Json.JsonConvert.DeserializeObject(response).ToString().Split(",");
+                    foreach (dynamic item in root)
+                    {
+                        string x = item.ToString();
+                        if (x.Contains("path")){
+                           rootpath = x.Split((char)34)[3].Replace("\\\\","\\");
+                        }
+                    }
+                    if (verbose) { Console.WriteLine(rootpath); Console.WriteLine(); };
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            url = $"{lidarrApiUrl}/api/v1/wanted/missing?pageSize=1000000000&includeArtist=true&monitored=true";
+            if (verbose) { Console.WriteLine(url); Console.WriteLine(); };
+            try
+            {
+                string response = client.DownloadString(url);
+                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(response);
+
+                if (verbose) { Console.WriteLine(data); Console.WriteLine(); };
+
+                if (data.records != null && data.records.Count > 0)
+                {
+                    Console.WriteLine(FiggleFonts.Ogre.Render("LidarrFusionCLI"));
+                    Console.WriteLine("By Angablade");
+                    foreach (var record in data.records)
+                    {
+                        if (record.artist.artistName != null && record.title != null)
+                        {
+                            string artist = record.artist.artistName;
+                            string album = record.title;
+                            string id = record.id;
+                            Funcs.PrintMinorHeader($"{artist} - {album} ({id})");
+                            string[] tracks = Funcs.GetTrackList(artist, album);
+                            try
+                            {
+                                if (tracks != null && tracks.Length > 0) { 
+                                    foreach (var track in tracks)
+                                    {
+                                        Console.WriteLine(track);
+
+                                        string youtubelink = Funcs.SearchYoutube($"{artist} - {album} - {track}{ytqappend}");
+                                        if (verbose) { Console.WriteLine(youtubelink); Console.WriteLine(); };
+
+                                        string tracklocation = Path.Combine(rootpath, Funcs.SanitizePath(artist), Funcs.SanitizePath(album), Funcs.SanitizePath(track) + ".mp3");
+
+                                        if (!Directory.Exists(Path.Combine(rootpath, Funcs.SanitizePath(artist), Funcs.SanitizePath(album))))
+                                        {
+                                            Directory.CreateDirectory(Path.Combine(rootpath, Funcs.SanitizePath(artist), Funcs.SanitizePath(album)));
+                                        }
+
+                                        Console.WriteLine(tracklocation);
+                                        string processPath = ytdlp;
+                                        string processArguments = $"{youtubelink} -x --audio-format \"mp3\" --match-filter \"is_live != true & was_live != true & duration < 3600\" --download-archive \"archive.txt\" --break-on-existing --output \"{tracklocation}\"";
+
+                                        if (generateListFlag)
+                                        {
+                                            File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, generateList), "yt-dlp_min.exe " + processArguments + Environment.NewLine);
+                                        }
+
+                                        if (autoDownload)
+                                        {
+                                            Console.WriteLine("Downloading: " + track);
+                                            Process process = new Process();
+                                            process.StartInfo.FileName = processPath;
+                                            process.StartInfo.Arguments = processArguments;
+                                            process.StartInfo.UseShellExecute = false;
+                                            process.StartInfo.RedirectStandardOutput = true;
+                                            process.StartInfo.RedirectStandardError = true;
+
+                                            process.Start();
+                                            string standardOutput = process.StandardOutput.ReadToEnd();
+
+                                            Console.WriteLine(standardOutput.Trim());
+                                            process.WaitForExit();
+
+                                            if (verbose)
+                                            {
+                                                string standardError = process.StandardError.ReadToEnd();
+                                                Console.WriteLine("Standard Error:");
+                                                Console.WriteLine(standardError);
+                                            };
+
+                                            if (tagger)
+                                            {
+                                                if (!standardOutput.Contains("has already been recorded in the archive"))
+                                                {
+                                                    Console.WriteLine("Writing Tags");
+                                                    Funcs.WriteID3v3Tags(tracklocation, artist, album, track);
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("Skipping Tags");
+                                                }
+                                            }
+                                            Console.WriteLine();
+                                        }
+                                    }
+                                }
+                            }
+                            catch { }
+                        }
+                        else
+                        {
+                                Console.WriteLine("Incomplete information for a missing album.");
+                        }
+                        Thread.Sleep(3500);
+                    }
+                }
+                else
+                {
+                    Funcs.PrintMinorHeader(response.ToString());
+                    Console.WriteLine("No missing tracks found on server or unable to parse the response.");
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
